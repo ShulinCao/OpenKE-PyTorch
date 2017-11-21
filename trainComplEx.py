69495eeda5707ce594bd42b4a8eba113da4b3d86
shelly-github <1120991665@qq.com>
2017-11-21 12:41:30 +0800

    add trainComplEx.py

---
+++ trainComplEx.py
@@ -0,0 +1,20 @@
+import config
+import models
+
+con = config.Config()
+con.set_in_path("./benchmarks/FB15K/")
+con.set_out_path("./benchmarks/FB15K/")
+con.set_work_threads(8)
+con.set_train_times(1000)
+con.set_nbatches(100)
+con.set_alpha(0.1)
+con.set_lmbda(0.0001)
+con.set_bern(1)
+con.set_dimension(100)
+con.set_margin(1)
+con.set_ent_neg_rate(1)
+con.set_rel_neg_rate(0)
+con.set_optimizer("Adagrad")
+con.init()
+con.set_model(models.ComplEx)
+con.train()
